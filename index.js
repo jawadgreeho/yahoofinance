cea73346bd9e4d6f91d82237cc7d6756be8ad1f7
jawadgreeho <jawad.greeho@gmail.com>
2021-12-19 12:15:04 +0600

    converted the code into async await

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const csv = require('csv-parser')
 const fs = require('fs');
+const util = require('util');
+const stream = require('stream');
 const yahooFinance = require('yahoo-finance');
 
 const app = express();
@@ -11,94 +13,83 @@ app.listen(3001, ()=>{
     console.log('server running on port 3001');
 });
 
+const pipeline = util.promisify(stream.pipeline);
+
 const results = [] 
 const companies = []
 const headers = []
 const output = []
 
-const getCompanies = (results) => {
+const getCompanies = async() => {
     for(let i = 0; i < results.length; i++){
      companies.push(results[i].company) 
     }
     console.log("printing companies", companies);
-    
-    createHeader()
- }
+    return companies
+}
 
 
-fs.createReadStream('companies.csv')
-    .pipe(csv(['company', 'origin']))
-    .on('data', (data) => results.push(data))
-    .on('end', () => {
-        // console.log(results);
-        getCompanies(results); 
-    })
+const getData = async() => {
+    try{
+        const result = await yahooFinance.historical({
+            symbols: companies,
+            from: '2012-01-01',
+            to: '2012-01-10',
+            period: 'd'  // 'd' (daily), 'w' (weekly), 'm' (monthly), 'v' (dividends only)
+        })
 
-    
-
-    const getData = async() => {
-        try{
-            const result = await yahooFinance.historical({
-                symbols: companies,
-                from: '2012-01-01',
-                to: '2012-01-10',
-                period: 'd'  // 'd' (daily), 'w' (weekly), 'm' (monthly), 'v' (dividends only)
-            })
-
-            console.log(result);
-            let j = true
-            for (const [key, value] of Object.entries(result)) {
-                for(let i = 0; i < value.length; i++){
-                    if(j) {
-                        output.push({Date: value?.[i]?.date.toISOString()})
-                    }
-                    console.log(`${key}: ${value?.[i]?.close.toFixed(3) ?? ""} ${value?.[i]?.date ?? ""}`);
-                    output[i][value?.[i]?.symbol] = value?.[i]?.close?.toFixed(3)
+        console.log(result);
+        let j = true
+        for (const [key, value] of Object.entries(result)) {
+            for(let i = 0; i < value.length; i++){
+                if(j) {
+                    output.push({Date: value?.[i]?.date.toISOString()})
                 }
-                j = false
-                console.log("output", output);
-              }
-        // console.log(result.'583.SI'[0].close);
-        console.log("printing output", output);
-        }
-        catch(err){
-            console.log(err);
-        }
+                console.log(`${key}: ${value?.[i]?.close.toFixed(3) ?? ""} ${value?.[i]?.date ?? ""}`);
+                output[i][value?.[i]?.symbol] = value?.[i]?.close?.toFixed(3)
+            }
+            j = false
+            console.log("output", output);
+            }
+    console.log("printing output", output);
+    return output
+    }
+    catch(err){
+        console.log(err);
+    }
+}
+
+    
+const createHeader = async() => {
+    headers.push({id: 'Date', title: 'Date'})
+    for (let i = 0; i < companies.length; i++){
+        headers.push({id: companies[i], title: companies[i]})
+        console.log("headers", headers);
+    }
+    return headers;
+}
+
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+const csvWriter = createCsvWriter({
+    path: 'output1.csv',
+    header: headers
+});
+  
+async function run() {
+            try{
+                fs.createReadStream('companies.csv')
+                .pipe(csv(['company', 'origin']))
+                .on('data', (data) => results.push(data))
+                .on('end', async() => {
+                    await getCompanies()
+                    await createHeader()
+                    await getData()
+                    csvWriter.writeRecords(output)
+                    console.log('Pipeline succeeded');
+                })
+            }catch(error){
+                console.log(error);
+            }
     }
 
-    
-    const createHeader = () => {
-        headers.push({id: 'Date', title: 'Date'})
-        for (let i = 0; i < companies.length; i++){
-            headers.push({id: companies[i], title: companies[i]})
-            console.log("headers", headers);
-        }
-    }
-
-    const createCsvWriter = require('csv-writer').createObjectCsvWriter;
-    const csvWriter = createCsvWriter({
-        path: 'output.csv',
-        header: headers
-    });
-    
-    
-    
-
-    
-
-    setTimeout(function(){ getData() }, 1000);
-    setTimeout(function(){ csvWriter.writeRecords(output)       // returns a promise
-        .then(() => {
-            console.log('...Done');
-        }); }, 15000);
-
-    
-// yahooFinance.historical({
-//     symbols: ['AAPL', 'GOOGL', '9E8.F',],
-//     from: '2012-01-01',
-//     to: '2012-01-10',
-//     period: 'd'  // 'd' (daily), 'w' (weekly), 'm' (monthly), 'v' (dividends only)
-// }, function (err, result) {
-//     if (err) { console.log(err); }
-//     console.log(result);
-//   });
+run()
